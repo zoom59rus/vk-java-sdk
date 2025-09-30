b6bb54cab685a1190b342c2dbeb2d00229c0141b
Nazarov Anton <naa@rio-soft.ru>
2025-09-30 17:05:04 +0500

    Ru zone

--- sdk/src/main/java/com/vk/api/sdk/client/VkApiClient.java
+++ sdk/src/main/java/com/vk/api/sdk/client/VkApiClient.java
@@ -9,9 +9,9 @@ import org.apache.commons.lang3.StringUtils;
 public class VkApiClient {
     private static final String API_VERSION = "5.199";
 
-    private static final String API_ADDRESS = "https://api.vk.com/method/";
+    private static final String API_ADDRESS = "https://api.vk.ru/method/";
 
-    private static final String OAUTH_ENDPOINT = "https://oauth.vk.com/";
+    private static final String OAUTH_ENDPOINT = "https://oauth.vk.ru/";
 
     private static final Integer DEFAULT_RETRY_ATTEMPTS_INTERNAL_SERVER_ERROR_COUNT = 3;
 
